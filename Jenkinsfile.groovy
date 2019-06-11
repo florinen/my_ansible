5b14d2c99382d97171cd249dd15cd95d6d97710f
florinen <nenfl24@gmail.com>
2019-06-11 16:54:11 -0500

    jenkins file created

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,35 @@
+pipeline {
+    agent any
+    stages {
+      stage('checkout') {
+           steps {
+           
+                git branch: 'master', url: 'https://github.com/florinen/my_ansible.git'
+             
+          }
+        }
+        
+           stage('Ansible Init') {
+            steps {
+                script {
+                
+               def tfHome = tool name: 'Ansible'
+                env.PATH = "${tfHome}:${env.PATH}"
+                 sh 'ansible --version'
+                    
+            }
+            }
+        }
+         
+        stage('Ansible Deploy') {
+             
+            steps {
+                 
+              dir('ansible/my_ansible')
+              {
+               
+               sh 'ansible all -m ping -i hosts'
+               
+            }
+            }
+        }
