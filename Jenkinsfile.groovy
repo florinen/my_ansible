22ac7662d3e0c45a5e81200c4696c03f33d0fc41
florinen <nenfl24@gmail.com>
2019-06-11 19:24:52 -0500

    updated sources

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,15 +2,14 @@ pipeline {
     agent any
     stages {
       stage('Checkout SCM') {
-        git branch: 'master', url: 'https://github.com/florinen/my_ansible.git'
-             
+          steps {
+              git branch: 'master', url: 'https://github.com/florinen/my_ansible.git'
+          }
+                    
         }
       stage('Ansible Deploy') {
-            
-          steps {
-                 
+           steps {
                sh 'ansible -i hosts -m ping all'
-               
             }
         }
     }
